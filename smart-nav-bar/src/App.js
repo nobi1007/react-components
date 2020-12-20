22bf1fc7432c30612d6c9047a3b6a5a7ae8fbf64
nobi1007 <mittalshyam1007@gmail.com>
2020-12-20 14:33:30 +0530

    add description of the smart-menubar-compoent

--- smart-nav-bar/src/App.js
+++ smart-nav-bar/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
 import FlexibleMenuBar from "./components/FlexibleMenuBar";
+import FlexibleMenuBarDirectons from "./components/FlexibleMenuBarDirectons";
 
 function App() {
   return (
     <div className="App">
       <FlexibleMenuBar eachItemWidth={40} eachItemPadding={4} />
+      <FlexibleMenuBarDirectons />
     </div>
   );
 }
---
+++ smart-nav-bar/src/components/FlexibleMenuBarDirectons/FlexibleMenuBarDirectons.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+import "./FlexibleMenuBarDirectons.scss";
+
+const FlexibleMenuBarDirectons = () => {
+  return (
+    <div className="flexible-menubar-directons-container">
+      <h2>
+        ! <span>Component Description and Usage</span> !
+      </h2>
+      <section className="main">
+        <ol type="a">
+          <li>
+            The <code>FlexibleMenuBar</code> container accepts each object's{" "}
+            <code>width</code> and <code>padding</code> / <code>margin</code>{" "}
+            applied to it as props.
+          </li>
+          <li>
+            Based on the total space available and total objects passed to the
+            component, it calculates how many objects can be displayed in the
+            menu bar.
+          </li>
+          <li>
+            Also, we may want some objects to displayed all the time and hence
+            we also have <code>leftConstData</code> and{" "}
+            <code>rightConstData</code>. These are <code>arrays</code> through
+            which we can pass the constant data.
+          </li>
+          <li>
+            And, we pass the variable objects/items to be displayed in the form
+            of array and can be declared in <code>allDataList</code>.
+          </li>
+          <li>
+            For, full logic of the component chekout{" "}
+            <a href="https://github.com/nobi1007/react-components/blob/main/smart-nav-bar/src/components/FlexibleMenuBar/FlexibleMenuBar.js">
+              here
+            </a>
+            .
+          </li>
+          <li>
+            And, do star and fork the repo if it was okay :) and let me know
+            about your thoughts in{" "}
+            <code>
+              <a href="https://github.com/nobi1007/react-components/issues/1">
+                Issues
+              </a>
+            </code>{" "}
+            section.
+          </li>
+        </ol>
+      </section>
+    </div>
+  );
+};
+
+export default FlexibleMenuBarDirectons;
